0cbc65da7a67289484b11a0abb3ef262af33bbdc
caballeroshie <caballeroshie@gmail.com>
2022-08-30 11:44:46 +0800

    initial commit

---
+++ bootcamp_todos/android/app/src/main/kotlin/com/example/todo_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.caballeroshie.tweet_clone
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ bootcamp_todos/lib/tweets/home/tweets_page.dart
@@ -0,0 +1,116 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
+import 'package:todo_app/tweets/posts/tweet_details_page.dart';
+import 'package:todo_app/tweets/posts/tweet_post_page.dart';
+
+import '../../widgets/item_button.dart';
+
+class TweetsPage extends StatefulWidget {
+  const TweetsPage({Key? key}) : super(key: key);
+
+  @override
+  State<TweetsPage> createState() => _TweetsPageState();
+}
+
+class _TweetsPageState extends State<TweetsPage> {
+
+  final tweetsCollection = FirebaseFirestore.instance.collection('tweets');
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Tweets'),
+      ),
+      body: Center(
+        child: Stack(
+          children: [
+            StreamBuilder<QuerySnapshot>(
+              stream: tweetsCollection.snapshots(),
+              builder: (context, snapshot) {
+                if (snapshot.hasData && snapshot.data!.docs.isNotEmpty) {
+                  return ListView.builder(
+                    itemCount: snapshot.data!.docs.length,
+                    shrinkWrap: true,
+                    itemBuilder: (context, index) {
+                      // Add this to read the document data
+                      final data = snapshot.data!.docs[index].data()
+                          as Map<String, dynamic>;
+                      return InkWell(
+                        onTap: () {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(builder: (_) {
+                              return TweetDetailsPage(
+                                id: snapshot.data!.docs[index].id,
+                                title: data['title'],
+                                description: data['description'],
+                                onItemDeleted: () async {
+                                  // Delete
+                                  await tweetsCollection 
+                                      .doc(snapshot.data!.docs[index].id)
+                                      .delete();
+                                },
+                              );
+                            }),
+                          );
+                        },
+                        child: Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              if (data['title'] is String)
+                                Text(
+                                  // Add this to read the title property
+                                  data['title'],
+                                  style: const TextStyle(
+                                    fontSize: 24,
+                                    fontWeight: FontWeight.bold,
+                                  ),
+                                ),
+                              if (data['description'] is String)
+                                Text(
+                                  // Add this to read the description property
+                                  data['description'],
+                                  style: const TextStyle(
+                                    fontSize: 16,
+                                  ),
+                                ),
+                            ],
+                          ),
+                        ),
+                      );
+                    },
+                  );
+                }
+
+                return const Center(
+                  child: Text('No items found.'),
+                );
+              },
+            ),
+            // const Center(
+            //   child: Text('Hello world!'),
+            // ),
+            Align(
+              alignment: Alignment.bottomCenter,
+              child: ItemButton(
+                title: 'New Item',
+                color: Colors.green,
+                onItemPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (_) => const TweetPostPage(),
+                    ),
+                  );
+                },
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
